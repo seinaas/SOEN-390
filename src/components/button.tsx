55b51b5fe16093ef9e0aa591827f254f325fce69
Seina Assadian <assadian.seina@gmail.com>
2023-01-28 18:24:32 -0500

    feat: [SOEN-42] Updated tests

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { type HTMLMotionProps, motion } from 'framer-motion';
+import { type HTMLProps } from 'react';
 
 type Props = {
   reverse?: boolean;
@@ -7,34 +6,27 @@ type Props = {
   fullWidth?: boolean;
   iconLeft?: JSX.Element;
   children?: React.ReactNode;
-} & HTMLMotionProps<'button'>;
+  type?: 'button' | 'submit' | 'reset';
+} & HTMLProps<HTMLButtonElement>;
 
 export const Button: React.FC<Props> = ({ reverse, variant = 'primary', fullWidth, iconLeft, ...props }) => {
-  let twStyle = '';
-  if (variant === 'primary') {
-    if (reverse) {
-      twStyle = 'bg-white border-white text-primary-600';
-    } else {
-      twStyle = 'bg-primary-600 border-primary-600 text-white';
-    }
-  } else {
-    if (reverse) {
-      twStyle = 'bg-transparent border-white text-white';
-    } else {
-      twStyle = 'bg-transparent border-primary-600 text-primary-600';
-    }
-  }
   return (
-    <motion.button
+    <button
       type='submit'
-      className={`flex items-center justify-center gap-x-3 rounded-md border-4 px-5 py-3 font-bold uppercase disabled:opacity-75 ${twStyle} ${
-        fullWidth ? 'w-full' : ''
-      }
+      className={`flex items-center justify-center gap-x-3 rounded-md border-4 px-5 py-3 font-bold uppercase disabled:opacity-75 ${
+        variant === 'primary'
+          ? reverse
+            ? 'border-white bg-white text-primary-600'
+            : 'border-primary-600 bg-primary-600 text-white'
+          : reverse
+          ? 'border-white bg-transparent text-white'
+          : 'border-primary-600 bg-transparent text-primary-600'
+      } ${fullWidth ? 'w-full' : ''}
       ${props.className || ''}`}
       {...props}
     >
       {iconLeft}
       <div className='flex items-center justify-center gap-x-1 overflow-hidden leading-[0.8]'>{props.children}</div>
-    </motion.button>
+    </button>
   );
 };
--- tests/cypress/component/Button.cy.tsx
+++ tests/cypress/component/Button.cy.tsx
@@ -19,4 +19,24 @@ describe('<Button />', () => {
     cy.dataCy('button1').should('have.class', 'w-full');
     cy.dataCy('button2').should('not.have.class', 'w-full');
   });
+  it('is transparent with a border when variant is secondary', () => {
+    cy.mount(<Button variant='secondary'>Hello!</Button>);
+    cy.contains('Hello!').should('have.class', 'border-primary-600 bg-transparent');
+  });
+  it('should have reverse colors when reverse is true', () => {
+    cy.mount(<Button reverse>Hello!</Button>);
+    cy.contains('Hello!').should('have.class', 'border-white bg-white');
+  });
+  it('should be secondary and have reverse colors when reverse and variant are true', () => {
+    cy.mount(
+      <Button reverse variant='secondary'>
+        Hello!
+      </Button>,
+    );
+    cy.contains('Hello!').should('have.class', 'border-white bg-transparent');
+  });
+  it('should have a left icon when iconLeft is passed', () => {
+    cy.mount(<Button iconLeft={<div data-cy='icon' />}>Hello!</Button>);
+    cy.dataCy('icon').should('exist');
+  });
 });
