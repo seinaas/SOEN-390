23825743c25ffe2ed1b86af157caa018cfbaa72e
Seina Assadian <assadian.seina@gmail.com>
2023-02-16 18:04:52 -0500

    feat: [SOEN-51] Fixed import error

--- src/pages/u/[email].tsx
+++ src/pages/u/[email].tsx
@@ -1,7 +1,7 @@
 import { type GetServerSidePropsContext } from 'next';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { Button } from '../../components/button';
+import Button from '../../components/button';
 import { api } from '../../utils/api';
 import { IoMdPeople, IoMdChatboxes, IoMdPersonAdd, IoMdMail, IoMdPhonePortrait } from 'react-icons/io';
 import { type NextPageWithLayout } from '../_app';
