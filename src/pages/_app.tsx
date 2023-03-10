d28a9a73a3e9d7c1b2402aca3778e14bee5fb2a9
Seina Assadian <assadian.seina@gmail.com>
2023-03-09 23:43:12 -0500

    feat: [SOEN-38] Created websocket server, initialized connection and helpers, and setup scaffolding for chat implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import { type AppProps, type AppType } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { type NextPage } from 'next';
-import { type ReactElement, type ReactNode } from 'react';
+import { useEffect, type ReactElement, type ReactNode } from 'react';
 import { type Session } from 'next-auth';
 
 import { api } from '../utils/api';
-import { initPusher } from '../utils/pusher';
+import { disconnectPusher, initPusher } from '../utils/pusher';
 
 import '../styles/globals.css';
 
@@ -23,6 +23,11 @@ export type MyAppProps = AppProps & {
 initPusher();
 const MyApp = (({ Component, pageProps: { session, ...pageProps } }: MyAppProps) => {
   const getLayout = Component.getLayout ?? ((page) => page);
+  useEffect(() => {
+    return () => {
+      disconnectPusher();
+    };
+  }, []);
   return <SessionProvider session={session as Session}>{getLayout(<Component {...pageProps} />)}</SessionProvider>;
 }) as AppType;
 export default api.withTRPC(MyApp);
