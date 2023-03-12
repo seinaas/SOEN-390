ae6ba1dcc2af745862e7ea79339377db4e383545
AfandiM <84986555+AfandiM@users.noreply.github.com>
2023-03-12 16:50:47 -0400

    Comp issue with tests

--- tests/server/routers/chat.test.ts
+++ tests/server/routers/chat.test.ts
@@ -7,7 +7,12 @@ describe('chat', () => {
 
       const trigger = jest.spyOn(request.ctx.pusher, 'trigger');
 
-      await request.caller.chat.submit({ message: 'Hello World' });
+      await request.caller.chat.sendMessage({
+        message: 'Hello World',
+        senderId: '1',
+        receiverId: '2',
+        conversationId: '1',
+      });
       expect(trigger).toHaveBeenCalledWith('test', 'test-event', { message: 'Hello World' });
     });
   });
