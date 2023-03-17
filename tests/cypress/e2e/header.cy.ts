2a52572e535e73da74476d37aed5f6ede29804c1
WillTrem <wt22@videotron.ca>
2023-03-17 13:11:00 -0400

    Removed redundancy in header e2e tests

--- tests/cypress/e2e/header.cy.ts
+++ tests/cypress/e2e/header.cy.ts
@@ -1,6 +1,8 @@
 describe('Header', () => {
-  it('should successfully search for a user by part of first name', () => {
-    const email = `testuser-${Cypress._.random(0, 1e6)}@test.com`;
+  let email = '';
+  //Before you run all the test, register the test user in the database.
+  beforeEach(() => {
+    email = `testuser-${Cypress._.random(0, 1e6)}@test.com`;
     cy.visit('/auth/register');
     cy.dataCy('email-input').type(email);
     cy.dataCy('password-input').type('testpassword');
@@ -12,29 +14,13 @@ describe('Header', () => {
     cy.dataCy('last-name-input').type('User');
 
     cy.dataCy('register-btn').click();
-
     cy.visit(`/u/${email}`);
-
+  });
+  it('should successfully search for a user by part of first name', () => {
     cy.dataCy('search-user-input').type('Tes');
     cy.dataCy('search-user-dropdown').should('contain', email);
   });
   it('should successfully search for a user by part of last name', () => {
-    const email = `testuser-${Cypress._.random(0, 1e6)}@test.com`;
-
-    cy.visit('/auth/register');
-    cy.dataCy('email-input').type(email);
-    cy.dataCy('password-input').type('testpassword');
-    cy.dataCy('confirm-password-input').type('testpassword');
-
-    cy.dataCy('register-btn').click();
-
-    cy.dataCy('first-name-input').type('Test');
-    cy.dataCy('last-name-input').type('User');
-
-    cy.dataCy('register-btn').click();
-
-    cy.visit(`/u/${email}`);
-
     cy.dataCy('search-user-input').type('Use');
     cy.dataCy('search-user-dropdown').should('contain', email);
   });
