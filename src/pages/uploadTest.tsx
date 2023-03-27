3f86e317fdd4887eccb323ea347b16e36e52a382
WillTrem <wt22@videotron.ca>
2023-03-27 15:27:28 -0400

    Configuration for uploading files to R2 buckets

---
+++ src/pages/uploadTest.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Upload from '../components/upload';
+
+const uploadTest: NextPage = () => {
+  return <Upload></Upload>;
+};
+
+export default uploadTest;
