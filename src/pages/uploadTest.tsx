0bc64d89ba107a0d877e0aadc5b40fd7dde13bac
Seina Assadian <assadian.seina@gmail.com>
2023-04-03 02:31:31 -0400

    Removed upload test file

--- src/pages/uploadTest.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next';
-import Upload from '../components/upload';
-
-const uploadTest: NextPage = () => {
-  return <Upload></Upload>;
-};
-
-export default uploadTest;
