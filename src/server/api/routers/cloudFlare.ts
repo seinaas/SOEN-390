909ee52ba40b8795f514b83dcba5c3c49ec7e0c4
Seina Assadian <assadian.seina@gmail.com>
2023-04-22 23:37:55 -0400

    Updated tests

--- src/server/api/routers/cloudFlare.ts
+++ src/server/api/routers/cloudFlare.ts
@@ -1,6 +1,7 @@
+/* istanbul ignore file */
 import { z } from 'zod';
 
-import { createTRPCRouter, publicProcedure, protectedProcedure } from '../trpc';
+import { createTRPCRouter, protectedProcedure } from '../trpc';
 
 import { S3Client, PutObjectCommand, GetObjectCommand, ListObjectsV2Command } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
